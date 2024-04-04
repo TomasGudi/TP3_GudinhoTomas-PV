5d519e56ef81d1bde8dafb4659ee24a8db9e5267
tomas <tomas@TomasGudi>
2024-04-04 17:41:31 -0300

    Initial Commit

---
+++ TP01_GudinhoTomas/src/ar/edu/unju/fi/ejercicio01/main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio01;
+
+public class main {
+	
+	public static void ejercicio01() {
+		float n1 = 3;
+		float n2 = 8;
+		float n3 = 2;
+		float n4 = 11;
+		float n5 = 1;
+		float promedio = (n1+n2+n3+n4+n5)/5;
+		System.out.println(promedio);
+	}
+	
+}
