fd091c0b21430ff420293995116380f0847e3e84
tomas <tomas@TomasGudi>
2024-04-09 18:03:03 -0300

    ejercicio04

---
+++ TP01_GudinhoTomas/src/ar/edu/unju/fi/ejercicio04/Main.java
@@ -0,0 +1,38 @@
+package ar.edu.unju.fi.ejercicio04;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 0,aux = 1,fac = 1;
+		boolean flag = false;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		do {
+			System.out.println("ingrese un numero entre 0 y 10: ");
+			 if(scanner.hasNextInt()) {
+				 num = scanner.nextInt();
+				 if(num>=0 && num<11) {
+					 flag = true;
+			 	}
+			 }
+			 else {
+				 scanner.next();
+				 flag = false;
+			 } 
+		}while(!flag);
+		
+		scanner.close();
+		 
+		if(num != 0 && num != 1) {
+			while (aux <= num) {
+				fac = fac*aux; 
+				aux = aux+1;
+			}
+		}
+		System.out.println("Su factorial es: " + fac);
+	}
+}
