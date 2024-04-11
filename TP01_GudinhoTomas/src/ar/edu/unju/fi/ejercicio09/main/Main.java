4b2628012ffc79916fa8b151b8d22ab2cc01aaa6
tomas <tomas@TomasGudi>
2024-04-10 23:35:50 -0300

    ejercicio10

--- TP01_GudinhoTomas/src/ar/edu/unju/fi/ejercicio09/main/Main.java
+++ TP01_GudinhoTomas/src/ar/edu/unju/fi/ejercicio09/main/Main.java
@@ -9,19 +9,23 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
+		int codigo, descuento, aux=0;
+		double precio;
+		String nombre;
 		
+		while(aux < 3) {
 		Producto producto = new Producto();
 		System.out.print("\nIngrese Nombre: ");
-        String nombre = scanner.nextLine();
+        nombre = scanner.nextLine();
         producto.setNombre(nombre);
         System.out.print("\nIngrese codigo: ");
-        int codigo = scanner.nextInt();
+        codigo = scanner.nextInt();
         producto.setCodigo(codigo);
         System.out.print("\nIngrese precio: ");
-        double precio = scanner.nextDouble();
+        precio = scanner.nextDouble();
         producto.setPrecio(precio);
         System.out.print("\nIngrese descuento entre 0 y 50: ");
-        int descuento = scanner.nextInt();
+        descuento = scanner.nextInt();
         while(descuento<0 || descuento>51) {
         	System.out.print("\nIngrese descuento entre 0 y 50: ");
         	descuento = scanner.nextInt();
@@ -32,5 +36,7 @@ public class Main {
         
         System.out.println(producto);
         System.out.println("Precio con decuento=" + producto.calcularDescuento());
+        aux++;
+		}
 	}
 }
