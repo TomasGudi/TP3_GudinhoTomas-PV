8edc48bf8abfc9dd20947bbabb6203e68c070886
tomas <tomas@TomasGudi>
2024-05-09 16:43:56 -0300

    TP3

---
+++ TP01_GudinhoTomas/src/ar/edu/unju/fi/ejercicio19/MAin.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio19;
+
+import java.util.Date;
+
+public class MAin {
+
+	public static void main(String[] args) {
+	Date f = new Date(1986,12,01);
+	System.out.println(f);
+}
+}
+
+
---
+++ tp3_GudiñoTomas/src/main/java/ar/edu/unju/fi/Tp3GudinoTomasApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3GudinoTomasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3GudinoTomasApplication.class, args);
+	}
+
+}
